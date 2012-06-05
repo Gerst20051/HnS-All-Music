dd5fbabfc2a28032c7d511827981730fe117574e
Andrew Gerst <gerst20051@gmail.com>
2012-06-04 20:19:10 -0400

    added setData function
    changed default album cover
    changed fake album cover to actual cover
    changed click handlers to live and on
    added getDimensions function

--- src/script.js
+++ src/script.js
@@ -2,32 +2,80 @@
 /* Toggle body class engage when right bar button is clicked */
 /* .music-playing and .music-paused classes get applied to .player and #engageView */
 /* .music-playing is also applied to that li item in the list all others have .music-paused class */
-/* How to calculate left position of #widgetContainer based on screen size? */
-/* How to calculate height of #mainContainer based on screen size? Width of .player, #mainContainer, and .jspContainer is based off of this height */
-/* When we set the width and height of mainContainer also set the width of .jspPane height is 11px less than #mainContainer */
-/* When we set the height of #mainContainer also set the height of .jspTrack */
-/* How to calculate negative top position of pane and drag? */
-/* Why choose dimensions of 618px when screen dimensions are availHeight: 760px, availWidth: 1280px */
-/* Available screen height = 699px Diff = 61px */
-/* Player height is 78px + 2px for border (760 - 80 = 680) Diff = 62 */
+
 /* Determine the pixel interval of change per second based off of the width of the buffer line */
 /* Skip back button is hidden by default and when the first song in list is playing */
 /* Slip forward button is shown by default and is hidden when the last song in list is playing. */
-/* Width of meta titles changes to width of titles - (width of right buttons - 26px) on hover */
+
+$.fn.setData = function(obj){
+	if (typeof obj != "object") return this;
+	return this.each(function(){
+		for (key in obj) {
+			$(this).data(key, obj[key]);
+		}
+	});
+};
 
 (function(){
 aC = {
 playlist: [],
+setData: function(obj){
+	for (key in json){
+		$('body').data(key, json[key])
+	}
+},
 loadPlaylist: function(){
-	if (aC.playlist.length == 0) console.log('Playlist is empty');
-	else {
+	if (aC.playlist.length > 0) {
 		var list = [];
 		$.each(aC.playlist, function(i,v){
-			list.push($("<li/>").text(v)[0]);
+			if (typeof v == "object") {
+				var html = $("<div/>").attr('class','ppbtn'),
+					itemlist = $("<ul/>").attr('class','track-info'),
+					item = $('<li class="duration">'+aC.niceDuration(v.duration)+'</li><li class="track-title">'+v.track+'</li><li class="artist">'+v.artist+'</li>');
+				list.push($("<li/>").attr('class','music-paused item playlist').setData(v).html(html).append(itemlist.append(item))[0]);
+			} else {
+				var name = v.split(" - ");
+				var html = $("<ul/>").attr('class','ppbtn'),
+					itemlist = $("<ul/>").attr('class','track-info'),
+					item = $('<li class="duration"></li><li class="track-title">'+name[0]+'</li><li class="artist">'+name[1]+'</li>');
+				list.push($("<li/>").attr('class','music-paused item playlist').html(html).append(itemlist.append(item))[0]);
+			}
 		});
+		$(".player .album-art .art").attr('src',aC.playlist[0].img);
+		$(".player .meta .titles").find(".track-name").text(aC.playlist[0].track).end().find(".artist-name").text(aC.playlist[0].artist);
 		$("#content").html(list);
 		$("#mainContainer").jScrollPane();
-	}
+		$(".jspPane").width($(".jspContainer").width());
+	} else console.log('Playlist is empty');
+	/*
+<li class="track-1tJkic0TURTCXw3rBhYgWu music-paused item playlist" data-id="0" data-duration="175">
+	<div class="ppbtn"></div>
+	<ul class="track-info">
+		<li class="duration">2:56</li>
+		<li class="track-title">1. What Would You Do?</li>
+		<li class="artist">City High</li>
+	</ul>
+</li>
+	*/
+},
+niceDuration: function(b){
+	var g = b / 60,
+		b = Math.floor(g),
+		g = Math.round(60 * (g - b));
+	60 == g && (b++, g = 0);
+	return 10 > g ? b + ":0" + g : b + ":" + g;
+},
+getDimensions: function(){
+	/* How to calculate left position of #widgetContainer based on screen size? */
+	/* How to calculate height of #mainContainer based on screen size? Width of .player, #mainContainer, and .jspContainer is based off of this height */
+	/* When we set the width and height of mainContainer also set the width of .jspPane height is 11px less than #mainContainer */
+	/* When we set the height of #mainContainer also set the height of .jspTrack */
+	
+	/* Why choose dimensions of 618px when screen dimensions are availHeight: 760px, availWidth: 1280px */
+	/* Available screen height = 699px Diff = 61px */
+	/* Player height is 78px + 2px for border (760 - 80 = 680) Diff = 62 */
+	var height = $(window).height(), width = $(window).width();
+	
 },
 showNotificationBar: function(a){
 	$("#notifBar #message")[0].innerHTML = a;
@@ -39,13 +87,14 @@ hideNotificationBar: function(){
 };
 
 $(document).ready(function(){
+	aC.getDimensions();
 	$.getJSON("playlist.json", function(a){
 		if ($.isArray(a.data)) {
 			aC.playlist = a.data;
 			aC.loadPlaylist();
 		} else console.log("Error loading playlist");
 	});
-	$("#notifBar .notifCloseButton").click(function(){
+	$("#notifBar .notifCloseButton").live('click',function(){
 		hideNotificationBar();
 	});
 	$("#widgetContainer").hover(function(){
@@ -57,10 +106,11 @@ $(document).ready(function(){
 			width: $(".player").width() - $('.player').outerHeight() - 8 - 8 - 19
 		}, 100)
 	});
-	$(".player .album-art-container").click(function(a){
+	$(".player .album-art-container").live('click',function(a){
 		alert("Trigger Play Pause");
 	});
-	$(".item").click(function(b){
+	$("#content").on('click','.item',function(b){
+		console.log($(this).data());
 		alert("Trigger Play Pause");
 	});
 });
